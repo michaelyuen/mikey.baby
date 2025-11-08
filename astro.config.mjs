cb48d2d797d0be6f24292a59bc0c6c1cb39a5059
my <iam@mikey.baby>
2025-11-08 12:04:10 -0500

    Update header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,34 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mikey.baby",
-  integrations: [react()]
+  integrations: [react()],
+  experimental: {
+    fonts: [
+      {
+        name: "Gaegu",
+        cssVariable: "--font-gaegu",
+        provider: fontProviders.google(),
+      },
+      {
+        name: "Merriweather",
+        cssVariable: "--font-merriweather",
+        provider: fontProviders.google(),
+      },
+      {
+        name: "Open Sans",
+        cssVariable: "--font-open-sans",
+        provider: fontProviders.google(),
+        weights: ["400", "700"],
+      },
+      {
+        name: "Pacifico",
+        cssVariable: "--font-pacifico",
+        provider: fontProviders.google(),
+      }
+    ]
+  }
 });
