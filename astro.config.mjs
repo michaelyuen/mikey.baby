386a64955d6ef3e0ef7c7c6ea7ae4bc70ebad5d6
my <iam@mikey.baby>
2025-11-15 20:50:48 -0500

    Add tailwind and tailwind/typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig, fontProviders } from "astro/config";
-
 import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -30,5 +30,8 @@ export default defineConfig({
         provider: fontProviders.google(),
       }
     ]
+  },
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
